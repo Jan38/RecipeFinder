cfde5f4ac3afe53bc864bbb69c2f4312bf1c8701
Analbella <annabella.hampelmann@gmail.com>
2025-07-10 21:20:31 +0200

    comments in english and tidied up

--- main.py
+++ main.py
@@ -4,39 +4,47 @@ from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.neighbors import NearestNeighbors
 import random
 
+# --- logic --- #
 df = pd.read_json('test.json')
 
-# Vektorisieren und in Matrix umwandeln. 1 = vorhanden, 0 = nicht vorhanden
+# vectorise and convert to matrix. 1 = existing, 0 = non-existing
 mlb = MultiLabelBinarizer()
 X = mlb.fit_transform(df['ingredients'])
 
-# Zutaten-Vokabular
+# ingredients-vocabulary
 vocab = set(mlb.classes_)
 
-knn = NearestNeighbors(n_neighbors=5, metric='cosine')  # cosine-Distanz funktioniert gut
+knn = NearestNeighbors(n_neighbors=5, metric='cosine')  # cosine-distance works best
 knn.fit(X)
 
+# random possible outputs for a 'no ingredient entered' error message
 rnd_err_msg = {"What do you want me to do now?",
                "I suggest: a grocery trip",
                "Time to sin and order take out after all",
                "Perhaps a water soup with some ice?"}
 
-#nach Zutaten fragen
+# asks for ingredients and finds the closest fitting neighbours
 def find_recipes():
+    # strip removes empty spaces
     user_input =entry.get().strip()
 
+    # if user input is empty (= no input/ingredients entered)
     if not user_input:
-        # da evtl die funny fehlermeldungen? hahahaha
+        # outputs a random error message from a list of possible funny responses
         output_text.set(random.choice(list(rnd_err_msg)))
         return
 
+    # creates a set of all with ',' seperated, entered ingredients, in lower case and without spaces
     user_ingredients = [x.strip().lower() for x in user_input.split(',') if x.strip()]
+    # boolean that checks, if any entered ingredient is not in our vocab set
     unknown = [x for x in user_ingredients if x not in vocab]
+    # if any unknown ingredient was found
     if unknown:
         output_text.set(f"Error: Unknown ingredient(s): {', '.join(unknown)}")
         return
 
     user_vector = mlb.transform([user_ingredients])
+    # finds best fitting recipes
     distances, indices = knn.kneighbors(user_vector)
 
     results = []
@@ -64,38 +72,3 @@ output_label = tk.Label(root, textvariable=output_text, justify="left", anchor="
 output_label.pack(padx=10, pady=10, fill='both')
 
 root.mainloop()
-
-
-# vorherige version
-#valid_input = False
-#while not valid_input:
-    #strip entfernt leerzeichen
- #   user_input = input("What ingredients do you have? (e.g. milk, sugar, eggs...)\n> ").strip()
-  #  if user_input:
-        #lower macht alles lowercase
-        #zutaten bei jedem komma trennen/split
-        #if z.strip() entfernt leere/leerzeichen-zutaten
-   #     user_ingredients = [x.strip().lower() for x in user_input.split(',') if x.strip()]
-        #nur wenn mindestens eine existierende zutat eingegeben wurde weitermachen
-    #    unknown = [x for x in user_ingredients if x not in vocab]
-     #   if unknown:
-      #      print(f"Error: Unknown ingredient: {', '.join(unknown)}")
-       # elif any(user_ingredients):
-        #    valid_input = True
-        #else:
-         #   print("Error: Input can't be empty.\n")
-
-#user_vector = mlb.transform([user_ingredients])
-
-#from sklearn.neighbors import NearestNeighbors
-
-#knn = NearestNeighbors(n_neighbors=5, metric='cosine')  # cosine-Distanz funktioniert gut
-#knn.fit(X)
-
-# Ähnlichste Rezepte finden
-#distances, indices = knn.kneighbors(user_vector)
-
-# Ergebnisse anzeigen
-#for i, idx in enumerate(indices[0]):
- #   similarity = 1 - distances[0][i]
-  #  print(f"{i+1}. {df.iloc[idx]['id']} (Score: {similarity:.2f})")
